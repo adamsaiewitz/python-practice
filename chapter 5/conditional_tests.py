62b6a54cf190915c2b12ddd10861f680d5f66ef6
adamsaiewitz <adam.saiewitz@gmail.com>
2018-11-21 23:01:44 -0500

    EOD update

---
+++ chapter 5/conditional_tests.py
@@ -0,0 +1,34 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+animal = 'dog'
+print("Is animal == 'dog'? I predict True.")
+print(animal == 'dog')
+
+print("\nIs animal == 'cat'? I predict False.")
+print(animal == 'cat')
+
+guitar = 'fender'
+print("Is guitar == 'fender'? I predict True.")
+print(guitar == 'fender')
+
+print("\nIs guitar == 'gibson'? I predict False.")
+print(guitar == 'gibson')
+
+sport = 'baseball'
+print("Is sport == 'baseball'? I predict True.")
+print(sport == 'baseball')
+
+print("\nIs sport == 'soccer'? I predict False.")
+print(sport == 'soccer')
+
+food = 'burger'
+print("Is food == 'burger'? I predict True.")
+print(food == 'burger')
+
+print("\nIs food == 'pizza'? I predict False.")
+print(food == 'pizza')
